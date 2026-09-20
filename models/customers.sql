7e9b6a74c76d225f3a36e665c5760de381f35c46
Bertrand PAGER <bpager@hotmail.com>
2026-09-20 15:20:03 +0000

    commit with orders, customers and payments

--- models/customers.sql
+++ models/customers.sql
@@ -1,61 +1,35 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
+{{ config(
+    materialized='table'
+) }}
 
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
+    select * from {{ ref('stg_orders') }}
 ),
 
-customer_orders as (
-
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
-),
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
+payment as (
+    select * from {{ ref('stg_payment') }}
 )
 
-select * from final
+select
+    orders.order_id,
+    customers.customer_id,
+    customers.first_name,
+    customers.last_name,
+    /*payment.payment_id,*/
+    /*count(orders.order_id) as number_of_orders,*/
+    /*min(orders.order_date) as first_order_date,
+    max(orders.order_date) as most_recent_order_date*/
+    count(payment.payment_id) as nb_of_payment,
+    sum(payment.amount) as sum_of_payment
+
+    from orders
+    left join customers 
+        on orders.customer_id = customers.customer_id
+    left join payment
+        on orders.order_id = payment.orderid
+
+    group by 1, 2, 3, 4
---
+++ models/stg_payment.sql
@@ -0,0 +1,10 @@
+select
+    id as payment_id,
+    orderid,
+    paymentmethod,
+    status,
+    amount,
+    created,
+    _batched_at
+
+from {{ source('stripe', 'payment') }}
